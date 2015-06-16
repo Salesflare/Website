4d99372b3525c6dffa3510b8c633e3360bf4105f
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2015-06-16 19:00:59 +0200

    rest of the pages and mobile menu done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+	$('.open-menu').click(function(){
+		$('.sf-menu-mobile').slideDown('slow');;
+	});
+
+	$('.close-menu').click(function(){
+		$('.sf-menu-mobile').slideUp('slow');
+	});
+});
