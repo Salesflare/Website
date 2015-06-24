7cac97f02d8a92415941fceb08c2edf6257e0f20
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2015-06-24 12:38:15 +0200

    website ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,38 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
 gulp.task('styles', function() {
 	gulp.src('sass/*.scss')
 		.pipe(sass({
 			errLogToConsole: true
 		}))
-		.pipe(gulp.dest('./css/'))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('scripts', function(){
+	gulp.src('js/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('html', function() {
+    gulp.src('*.html')
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('img', function(){
+	gulp.src('img/*')
+	.pipe(gulp.dest('dist/img'));
+});
+
+
+gulp.task('fonts', function(){
+	gulp.src('fonts/*')
+	.pipe(gulp.dest('dist/fonts'));
 });
 
 //Watch task
-gulp.task('default',function() {
-	gulp.watch('sass/**/*.scss',['styles']);
-});
+gulp.task('default', ['styles', 'scripts', 'html', 'fonts', 'img']);
