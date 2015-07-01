cd010593bd7b16b7e166c05b0da6c6c5141ec835
Lieven Janssen <lieven.janssen@gmail.com>
2015-07-01 17:35:00 +0200

    fixed menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-var app = angular.module('Website', ['menuController']);
-app.controller('menuController', ['$scope', function($scope){
-		$scope.menu = 'close';
-}])
+$('document').ready(function(){
+	$('.open-menu').click(function(){
+		$('.sf-menu-mobile').slideDown('slow');;
+	});
+
+	$('.close-menu').click(function(){
+		$('.sf-menu-mobile').slideUp('slow');
+	});
+});
