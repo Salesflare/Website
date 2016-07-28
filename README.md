e251cf862e0cddc91ce552353fddc32b8e3d98de
Lieven Janssen <lieven.janssen@gmail.com>
2016-07-28 16:20:45 +0200

    Update 2016-07-28T14:20:14.352Z

--- README.md
+++
@@ -1,8 +0,0 @@
-# Website
-The website of Salesflare
-
-=============
-
-- If you don't have [Node.js/npm](http://nodejs.org) install it from [here](http://nodejs.org/download/)
-- Install via `$ npm install`
-- Build the website via the command `gulp`
--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-$('document').ready(function(){
-	$('.open-menu').click(function(){
-		$('.sf-menu-mobile').slideDown('slow');;
-	});
-
-	$('.close-menu').click(function(){
-		$('.sf-menu-mobile').slideUp('slow');
-	});
-});
+$("document").ready(function(){$(".open-menu").click(function(){$(".sf-menu-mobile").slideDown("slow")}),$(".close-menu").click(function(){$(".sf-menu-mobile").slideUp("slow")})});
