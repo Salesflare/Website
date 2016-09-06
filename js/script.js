63849ffa4eb7654b4049031167978289906adf87
Lieven Janssen <lieven.janssen@gmail.com>
2016-09-06 18:39:17 +0200

    popup dialog work in progress

--- js/script.js
+++ js/script.js
@@ -6,4 +6,22 @@ $('document').ready(function(){
 	$('.close-menu').click(function(){
 		$('.sf-menu-mobile').slideUp('slow');
 	});
+
+    $('#myModal').on('show.bs.modal', function (event) {
+      var modal = $(this);
+      modal.find('img').attr("src", "img/" + campaign + ".png");
+    })
+
+	if(campaign === 'investors-tc' || campaign === 'press-tc') {
+	   $('#myModal').modal('show');
+    };
 });
+
+var source = querystring('utm_source');
+var campaign = querystring('utm_campaign');
+
+function querystring(key) {
+   var re=new RegExp('(?:\\?|&)'+key+'=(.*?)(?=&|$)','gi');
+   var m=re.exec(document.location.search);
+   return m[1];
+}
