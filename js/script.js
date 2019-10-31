2023b3f5576dafb4e9c49d80e561b219c4df4f6b
Adri Van Houdt <adri.van.houdt@gmail.com>
2019-10-31 17:02:35 +0100

    Update 2019-10-31T16:02:32.838Z

--- js/script.js
+++ js/script.js
@@ -1,21 +1,2 @@
-$('document').ready(function(){
-	$('.animated-gif').gifplayer();
-
-	if ($('.animated-gif').visible()) {
-		$('.animated-gif').gifplayer('play');
-	}
-
-	$('.open-menu').click(function(){
-		$('.sf-menu-mobile').slideDown('slow');;
-	});
-
-	$('.close-menu').click(function(){
-		$('.sf-menu-mobile').slideUp('slow');
-	});
-
-    $(window).scroll(function(){
-		if ($('.animated-gif').visible()) {
-			$('.animated-gif').gifplayer('play');
-		}
-    });
-});
+$("document").ready(function(){$(".animated-gif").gifplayer(),$(".animated-gif").visible()&&$(".animated-gif").gifplayer("play"),$(".open-menu").click(function(){$(".sf-menu-mobile").slideDown("slow")}),$(".close-menu").click(function(){$(".sf-menu-mobile").slideUp("slow")}),$(window).scroll(function(){$(".animated-gif").visible()&&$(".animated-gif").gifplayer("play")})});
+//# sourceMappingURL=script.js.map
