3b7e3fb5fc3a84c3699898ea326198dfc8e74f9c
Kevin De Clerck <kevin@salesflare.com>
2021-05-03 15:35:48 +0200

    Update 2021-05-03T13:35:44.211Z

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-$("document").ready(function(){$(".animated-gif").gifplayer(),$(".animated-gif").visible()&&$(".animated-gif").gifplayer("play"),$(".open-menu").click(function(){$(".sf-menu-mobile").slideDown("slow")}),$(".close-menu").click(function(){$(".sf-menu-mobile").slideUp("slow")}),$(window).scroll(function(){$(".animated-gif").visible()&&$(".animated-gif").gifplayer("play")})});
+$("document").ready(function(){$(".open-menu").click(function(){$(".sf-menu-mobile").slideDown("slow")}),$(".close-menu").click(function(){$(".sf-menu-mobile").slideUp("slow")})});
 //# sourceMappingURL=script.js.map
